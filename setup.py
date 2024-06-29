f40c060bfe7dff7eac33283100379771b36cf6dc
Karthik Mothiki <62557178+KarthikMothiki@users.noreply.github.com>
2024-06-29 19:54:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,15 @@ setup(
     packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
+    maintainer='Karthik',
     maintainer_email='your_email@example.com',
-    description='Examples of minimal publisher/subscriber using rclpy',
+    description='Examples of publisher/subscriber using rclpy',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'publisher_node = my_pub_sub.publisher_node:main',
-            'subscriber_node = my_pub_sub.subscriber_node:main',
-            'publisher_counter = my_pub_sub.publisher_counter:main',
-            'subscriber_counter = my_pub_sub.subscriber_counter:main',
+            'my_pub = my_pub_sub.my_pub:main',
+            'my_sub = my_pub_sub.my_sub:main',
         ],
     },
 )
