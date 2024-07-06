34bbe90c62bd8500982e9cfa5f28aaa2b45a91a0
Karthik Mothiki <62557178+KarthikMothiki@users.noreply.github.com>
2024-07-07 00:44:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,24 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/pub_sub_launch.xml']),
+        ('share/' + package_name + '/launch', [
+            'launch/pub_sub_launch.xml',
+            'launch/service_client_launch.xml'
+        ]),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='your_name',
     maintainer_email='your_email@example.com',
-    description='Examples of ROS2 publishers and subscribers in Python',
+    description='Examples of ROS2 publishers, subscribers, services, and clients in Python',
     license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'publisher_counter = my_pub_sub.publisher_counter:main',
             'subscriber_counter = my_pub_sub.subscriber_counter:main',
+            'service = my_service_pkg.service:main',
+            'client = my_service_pkg.client:main',
         ],
     },
 )
