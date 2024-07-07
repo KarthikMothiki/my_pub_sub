1f77b884ca92b61177ce48cbe49e9237cea7719b
Karthik Mothiki <62557178+KarthikMothiki@users.noreply.github.com>
2024-07-07 22:24:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', [
-            'launch/pub_sub_launch.xml',
-            'launch/service_client_launch.xml'
-        ]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+        ],
     ],
     install_requires=['setuptools'],
     zip_safe=True,
